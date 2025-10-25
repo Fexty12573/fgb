bceed14d82b8f073f3ffa096b2a474aa997dc569
Fexty12573 <bogofogo1@gmail.com>
2025-10-26 01:23:17 +0200

    Fix some timer edge cases with TIMA/TMA

--- lib/timer.c
+++ lib/timer.c
@@ -20,6 +20,8 @@ static const uint16_t div_bit_table[] = {
     [TIMER_CLOCK_16384_HZ]  = (1 << 7), // Bit 7
 };
 
+static void fgb_timer_increment(fgb_timer* timer);
+
 void fgb_timer_init(fgb_timer* timer, fgb_cpu* cpu) {
     memset(timer, 0, sizeof(fgb_timer));
     timer->divider = 0xAC00; // Initial value for the divider register
@@ -32,20 +34,22 @@ void fgb_timer_tick(fgb_timer* timer) {
 
     const uint16_t div_bit = div_bit_table[timer->clk_sel];
     if ((prev_div & div_bit) != 0 && (timer->divider & div_bit) == 0 && timer->enable) {
-        timer->counter++;
-
-        if (timer->counter == 0x00) { // Overflow
-            timer->counter = timer->modulo;
-
-            fgb_cpu_request_interrupt(timer->cpu, IRQ_TIMER);
-        }
+        fgb_timer_increment(timer);
     }
 }
 
 void fgb_timer_write(fgb_timer* timer, uint16_t addr, uint8_t value) {
+	const uint16_t div_bit = div_bit_table[timer->clk_sel];
+
     switch (addr) {
     case TIMER_DIV_ADDRESS:
-        timer->divider = 0; 
+        // If the watched bit is set in the divider it will be cleared,
+		// causing a timer increment
+        if (timer->enable && (timer->divider & div_bit)) {
+            fgb_timer_increment(timer);
+		}
+
+        timer->divider = 0;
         break;
 
     case TIMER_TIMA_ADDRESS:
@@ -56,9 +60,16 @@ void fgb_timer_write(fgb_timer* timer, uint16_t addr, uint8_t value) {
         timer->modulo = value;
         break;
 
-    case TIMER_TAC_ADDRESS:
+    case TIMER_TAC_ADDRESS: {
+		const bool high = (timer->divider & div_bit) != 0;
         timer->control = value;
-        break;
+
+        // If the previous watched bit is set but the new one isn't, that will
+		// result in a negative edge and thus a timer increment
+        if (timer->enable && high && (timer->divider & div_bit_table[timer->clk_sel]) == 0) {
+            fgb_timer_increment(timer);
+		}
+    } break;
 
     default:
         log_warn("Unknown address for timer write: 0x%04X", addr);
@@ -83,5 +94,15 @@ uint8_t fgb_timer_read(const fgb_timer* timer, uint16_t addr) {
     default:
         log_warn("Unknown address for timer read: 0x%04X", addr);
         return 0xAA; // Return a default value for unknown addresses
+    }
+}
+
+void fgb_timer_increment(fgb_timer* timer) {
+    timer->counter++;
+
+    if (timer->counter == 0x00) { // Overflow
+        timer->counter = timer->modulo;
+
+        fgb_cpu_request_interrupt(timer->cpu, IRQ_TIMER);
     }
 }
